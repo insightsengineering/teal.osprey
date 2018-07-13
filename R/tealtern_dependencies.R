8855f7550911718a34cfe3583fbea772995f217c
Zhang, Carolyn {MDBB~South San Francisco} <zhang.carolyn@gene.com>
2018-07-13 23:50:21 +0200

    add teal.tern functions to dpendencies file

--- R/tealtern_dependencies.R
+++ R/tealtern_dependencies.R
@@ -1,1 +1,16 @@
-teal::get_rcode_header
+#import teal functions
+get_rcode_header <- teal:::get_rcode_header
+
+#import teal.tern functions
+validate_has_data <- teal.tern:::validate_has_data
+has_source_attribute <- teal.tern:::has_source_attribute
+whiteSmallWell <- teal.tern:::whiteSmallWell
+remove_enclosing_curly_braces <- teal.tern:::remove_enclosing_curly_braces
+validate_standard_inputs <- teal.tern:::validate_standard_inputs
+validate_has_data <- teal.tern:::validate_has_data
+validate_one_row_per_id <- teal.tern:::validate_one_row_per_id
+validate_in <- teal.tern:::validate_in
+validate_has_elements <- teal.tern:::validate_has_elements 
+validate_no_intersection <- teal.tern:::validate_no_intersection
+validate_has_variable <- teal.tern:::validate_has_variable
+validate_n_levels <- teal.tern:::validate_n_levels
