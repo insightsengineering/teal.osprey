edc34fb516b4f4c0ef2549ef1aeb6c924722df77
Sebastian Wolf <sebastian.wolf.sw1@roche.com>
2019-08-09 16:36:30 +0200

    styler

--- R/tm_t_ae_ctc.R
+++ R/tm_t_ae_ctc.R
@@ -30,13 +30,13 @@
 #' ASL <- rADSL %>% mutate(USUBJID = SUBJID)
 #' AAE <- rADAE %>% mutate(flag1 = ifelse(SEX == "F", "Y", "N")) %>% mutate(USUBJID = SUBJID)
 #'
-#' app <- teal::init(
+#' app <- init(
 #'   data = cdisc_data(ASL = ASL, AAE = AAE, code = '
-#'     ASL <- rADSL %>% mutate(USUBJID = SUBJID)
-#'     AAE <- rADAE %>%
-#'       mutate(flag1 = ifelse(SEX == "F", "Y", "N")) %>%
-#'       mutate(USUBJID = SUBJID)
-#'   '),
+#' ASL <- rADSL %>% mutate(USUBJID = SUBJID)
+#' AAE <- rADAE %>%
+#' mutate(flag1 = ifelse(SEX == "F", "Y", "N")) %>%
+#' mutate(USUBJID = SUBJID)
+#' '),
 #'   modules = root_modules(
 #'     tm_t_ae_ctc(
 #'       label = "Adverse Events Table By Highest NCI CTCAE Grade",
@@ -46,26 +46,23 @@
 #'       class_var = choices_selected(selected = "AEBODSYS", choices = c("AEBODSYS", "DEFAULT")),
 #'       term_var = choices_selected(selected = "AEDECOD", choices = c("AEDECOD", "DEFAULT")),
 #'       total_col = TRUE
-#'    )
+#'     )
 #'   )
 #' )
-#'
 #' \dontrun{
 #' shinyApp(app$ui, app$server)
 #' }
 #'
-#'
 tm_t_ae_ctc <- function(label,
-                    dataname,
-                    filter_var = NULL,
-                    arm_var,
-                    class_var,
-                    term_var,
-                    toxgr_var = "AETOXGR",
-                    total_col = TRUE,
-                    pre_output = NULL,
-                    post_output = NULL) {
-
+                        dataname,
+                        filter_var = NULL,
+                        arm_var,
+                        class_var,
+                        term_var,
+                        toxgr_var = "AETOXGR",
+                        total_col = TRUE,
+                        pre_output = NULL,
+                        post_output = NULL) {
   stopifnot(is.character.single(label))
   stopifnot(is.character.single(dataname))
   stopifnot(is.null(filter_var) || is.choices_selected(filter_var))
@@ -82,26 +79,27 @@ tm_t_ae_ctc <- function(label,
     server = srv_t_ae_ctc,
     ui = ui_t_ae_ctc,
     ui_args = args,
-    server_args = list(dataname = dataname,
-                       toxgr_var = toxgr_var),
+    server_args = list(
+      dataname = dataname,
+      toxgr_var = toxgr_var
+    ),
     filters = dataname
   )
-
 }
 
 ui_t_ae_ctc <- function(id, ...) {
-
   ns <- NS(id)
   a <- list(...)
 
   standard_layout(
     output = whiteSmallWell(uiOutput(ns("table"))),
-    encoding =  div(
-      tags$label("Encodings", class="text-primary"),
+    encoding = div(
+      tags$label("Encodings", class = "text-primary"),
       helpText("Analysis data:", tags$code(a$dataname)),
       optionalSelectInput(ns("filter_var"),
-                          label = div("Preset Data Filters", tags$br(), helpText("Observations with value of 'Y' for selected variable(s) will be used for analysis")),
-                          choices = a$filter_var$choices, selected = a$filter_var$selected, multiple = TRUE),
+        label = div("Preset Data Filters", tags$br(), helpText("Observations with value of 'Y' for selected variable(s) will be used for analysis")),
+        choices = a$filter_var$choices, selected = a$filter_var$selected, multiple = TRUE
+      ),
       optionalSelectInput(ns("arm_var"), "Arm Variable", a$arm_var$choices, a$arm_var$selected, multiple = FALSE),
       optionalSelectInput(ns("class_var"), "Class Variables", a$class_var$choices, a$class_var$selected, multiple = FALSE),
       optionalSelectInput(ns("term_var"), "Term Variables", a$term_var$choices, a$term_var$selected, multiple = FALSE),
@@ -111,7 +109,6 @@ ui_t_ae_ctc <- function(id, ...) {
     pre_output = a$pre_output,
     post_output = a$post_output
   )
-
 }
 
 srv_t_ae_ctc <- function(input, output, session, datasets, dataname, toxgr_var) {
@@ -119,7 +116,6 @@ srv_t_ae_ctc <- function(input, output, session, datasets, dataname, toxgr_var) 
   init_chunks()
 
   output$table <- renderUI({
-
     ASL_FILTERED <- datasets$get_data("ASL", reactive = TRUE, filtered = TRUE)
     AAE_FILTERED <- datasets$get_data(dataname, reactive = TRUE, filtered = TRUE)
 
@@ -140,13 +136,13 @@ srv_t_ae_ctc <- function(input, output, session, datasets, dataname, toxgr_var) 
 
     chunks_push(bquote({
       ANL <- .(as.name(aae_name)) %>%
-          select(.(anl_vars))
+        select(.(anl_vars))
     }))
 
     if (!is.null(filter_var)) {
       chunks_push(bquote(
-          ANL <- quick_filter(.(filter_var), ANL) %>%
-              droplevels()
+        ANL <- quick_filter(.(filter_var), ANL) %>%
+          droplevels()
       ))
     }
 
@@ -160,17 +156,17 @@ srv_t_ae_ctc <- function(input, output, session, datasets, dataname, toxgr_var) 
     chunks_push_new_line()
 
     total <- if (isTRUE(all_p)) {
-          "All Patients"
-        } else {
-          NULL
-        }
+      "All Patients"
+    } else {
+      NULL
+    }
 
     chunks_eval()
 
     chunks_push(call(
       "t_ae_ctc_v2",
-      class = bquote(ANL[,.(class_var)]),
-      term = bquote(ANL[,.(term_var)]),
+      class = bquote(ANL[, .(class_var)]),
+      term = bquote(ANL[, .(term_var)]),
       id = bquote(ANL$USUBJID),
       grade = bquote(ANL$TOXGR),
       col_by = bquote(droplevels(as.factor(ANL[[.(arm_var)]]))),
@@ -187,12 +183,11 @@ srv_t_ae_ctc <- function(input, output, session, datasets, dataname, toxgr_var) 
 
   observeEvent(input$show_rcode, {
     show_rcode_modal(
-        title = "Adverse Events Table By Highest NCI CTCAE Grade",
-        rcode = get_rcode(
-            datasets = datasets,
-            title = "Adverse Events Table By Highest NCI CTCAE Grade"
-        )
+      title = "Adverse Events Table By Highest NCI CTCAE Grade",
+      rcode = get_rcode(
+        datasets = datasets,
+        title = "Adverse Events Table By Highest NCI CTCAE Grade"
+      )
     )
   })
-
 }
