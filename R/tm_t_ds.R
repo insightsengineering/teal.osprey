6d1a3abe7b309f024ea109474b7cbf8cc8ad6709
Sebastian Wolf <sebastian.wolf.sw1@roche.com>
2019-08-13 13:49:08 +0200

    made tm_t_ds work in master

--- R/tm_t_ds.R
+++ R/tm_t_ds.R
@@ -18,46 +18,40 @@
 #' @examples
 #'
 #' \dontrun{
-#' #Example using random dataset
-#'
+#' # Example using stream (ADaM) dataset
 #' library(dplyr)
 #'
-#' data("rADSL")
-#' ASL <- rADSL
+#' ASL <- rADSL %>% mutate(USUBJID = SUBJID)
 #'
-#' x <- teal::init(
-#'   data = list(ASL = ASL),
+#' app <- init(
+#'   data = cdisc_data(
+#'     ASL = ASL,
+#'     code = "ASL <- rADSL %>% mutate(USUBJID = SUBJID)",
+#'     check = FALSE
+#'   ),
 #'   modules = root_modules(
 #'     tm_t_ds(
 #'        label = "Patient Disposition Table",
 #'        dataname = "ASL",
-#'        arm_var = "ARM",
-#'        arm_var_choices = c("ARM", "ARMCD"),
-#'        class_var = "EOSSTT",
-#'        class_var_choices = "EOSSTT",
-#'        term_var = "DCSREAS",
-#'        term_var_choices = c("DCSREAS", "DCSREASP"),
+#'        arm_var = choices_selected(selected = "ARM", choices = c("ARM", "ARMCD")),
+#'        class_var =  choices_selected(selected = "EOSSTT", choices = "EOSSTT"),
+#'        term_var = choices_selected(selected = "DCSREAS", choices = c("DCSREAS", "DCSREASP")),
 #'        total_col = TRUE
-#'    )
+#'     )
 #'   )
 #' )
 #'
 #' shinyApp(x$ui, x$server)
 #' }
 #'
-#'
 tm_t_ds <- function(label,
                     dataname,
                     arm_var,
-                    arm_var_choices,
                     class_var,
-                    class_var_choices,
                     term_var,
-                    term_var_choices,
                     total_col = TRUE,
                     pre_output = NULL,
-                    post_output = NULL,
-                    code_data_processing = NULL) {
+                    post_output = NULL) {
 
   args <- as.list(environment())
 
@@ -82,9 +76,11 @@ ui_t_ds <- function(id, ...) {
     encoding =  div(
       tags$label("Encodings", class="text-primary"),
       helpText("Analysis data:", tags$code(a$dataname)),
-      optionalSelectInput(ns("arm_var"), "Arm Variable", a$arm_var_choices, a$arm_var, multiple = FALSE),
-      optionalSelectInput(ns("class_var"), "Class Variable", a$class_var_choices, a$class_var, multiple = FALSE),
-      optionalSelectInput(ns("term_var"), "Term Variable", a$term_var_choices, a$term_var, multiple = FALSE),
+      optionalSelectInput(ns("arm_var"), "Arm Variable", a$arm_var$choices, a$arm_var$selected, multiple = FALSE),
+      optionalSelectInput(ns("class_var"), "Class Variable", a$class_var$choices, a$class_var$selected,
+        multiple = FALSE),
+      optionalSelectInput(ns("term_var"), "Term Variable", a$term_var$choices, a$term_var$selected,
+        multiple = FALSE),
       checkboxInput(ns("All_Patients"), "Add All Patients", value = a$total_col)
     ),
     forms = actionButton(ns("show_rcode"), "Show R Code", width = "100%"),
@@ -94,106 +90,64 @@ ui_t_ds <- function(id, ...) {
 
 }
 
-srv_t_ds <- function(input, output, session, datasets, dataname, code_data_processing) {
+srv_t_ds <- function(input, output, session, datasets, dataname) {
 
-  chunks <- list(
-    vars = "# Not Calculated",
-    data = "# Not Calculated",
-    analysis = "# Not Calculated"
-  )
-
+  init_chunks()
 
   output$table <- renderUI({
 
     ASL_FILTERED <- datasets$get_data("ASL", reactive = TRUE, filtered = TRUE)
+
+    chunks_reset(envir = environment())
 
     arm_var <- input$arm_var
     class_var <- input$class_var
     term_var <- input$term_var
     all_p <- input$All_Patients
 
-    # # validate your input values
-    # validate_standard_inputs(
-    #   ASL = ASL_FILTERED,
-    #   aslvars = c("USUBJID", arm_var, class_var, term_var)
-    # )
-
     asl_vars <- unique(c("STUDYID","USUBJID", arm_var, class_var, term_var))
 
-    chunks$vars <<- bquote({
-      arm_var <- .(arm_var)
-      class_var <- .(class_var)
-      term_var <- .(term_var)
-      all_p <- .(all_p)
-      asl_vars <- .(asl_vars)
+    if(all_p == TRUE){
+      total <- "All Patients"
+    } else {
+      total <- NULL
+    }
+    chunks_push(bquote({
+      ASL_f <- as.data.frame(ASL_FILTERED[, .(asl_vars)])
+    }))
 
-    })
+    chunks_eval()
 
-    chunks$data <<- bquote({
-      ASL_f <- ASL_FILTERED
+    # check dataset
+    validate_has_data(chunks_get_var("ASL_f"), min_nrow = 1)
+    validate(need(chunks_get_var("ASL_f")[[arm_var]], "Arm variable does not exist"))
+    validate(need(!("" %in% chunks_get_var("ASL_f")[[arm_var]]), "arm values can not contain empty strings ''"))
 
-      {if(all_p == TRUE) {
-        total = "All Patients"
-      } else {
-        total = NULL
-      }}
-
-      ASL_f <- ASL_f[, .(asl_vars)] %>%
-        as.data.frame()
-
-    })
-
-    eval(chunks$data)
-
-    validate_has_data(ASL_f, min_nrow = 1)
-    validate(need(ASL_f[[arm_var]], "Arm variable does not exist"))
-    validate(need(!("" %in% ASL_f[[arm_var]]), "arm values can not contain empty strings ''"))
-
-    chunks$analysis <<- call(
+    chunks_push(call(
       "t_ds",
-      class = bquote(ASL_f[, class_var]),
-      term = bquote(ASL_f[, term_var]),
+      class = bquote(ASL_f[, .(class_var)]),
+      term = bquote(ASL_f[, .(term_var)]),
       id = bquote(ASL_f$USUBJID),
       col_by = bquote(droplevels(as.factor(ASL_f[[.(arm_var)]]))),
       total = total
-    )
+    ))
 
-    tbl <- try(eval(chunks$analysis))
+    tbl <- chunks_eval()
 
-    if (is(tbl, "try-error")) validate(need(FALSE, paste0("could not calculate the table:\n\n", tbl)))
+    if (!chunks_is_ok()) validate(need(FALSE, paste0("could not calculate the table:\n\n", tbl)))
 
     as_html(tbl)
   })
 
 
-
   observeEvent(input$show_rcode, {
-
-    header <- get_rcode_header_osprey(
-      title = "Patient Disposition Table",
-      datanames = dataname,
-      datasets = datasets,
-      ""
+    show_rcode_modal(
+        title = "Disposition table",
+        rcode = get_rcode(
+            datasets = datasets,
+            title = "R Code for the Current AE Overview Table"
+        )
     )
-
-    str_rcode <- paste(c(
-      "",
-      header,
-      "",
-      remove_enclosing_curly_braces(deparse(chunks$vars, width.cutoff = 60)),
-      "",
-      remove_enclosing_curly_braces(deparse(chunks$data, width.cutoff = 60)),
-      "",
-      remove_enclosing_curly_braces(deparse(chunks$analysis, width.cutoff = 60))
-    ), collapse = "\n")
-
-    # .log("show R code")
-    showModal(modalDialog(
-      title = "R Code for the Current Patient Disposition Table",
-      tags$pre(tags$code(class="R", str_rcode)),
-      easyClose = TRUE,
-      size = "l"
-    ))
   })
 
 }
