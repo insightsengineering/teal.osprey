d3ff5d2efcdd9d66e05d368eb8abe6e4f1dcdbe3
Sorin Voicu <61691256+sorinvoicu@users.noreply.github.com>
2021-08-11 17:44:54 +0300

    readme, licence, other Roche refs (#18)
    * readme, licence, other Roche refs
    
    * add README to WORDLIST
    
    * removing agileR refs

--- R/teal.osprey.R
+++ R/teal.osprey.R
@@ -1,7 +1,6 @@
 #' teal.osprey: Enabling you to write modules in teal
 #'
 #' The \code{teal.osprey} R package contains interactive \code{\link{teal}} modules for the outputs
-#' (TLGs) in \url{http://go.roche.com/agile-R}
 #'
 #' @import dplyr osprey shiny teal teal.devel utils.nest
 #' @docType package
