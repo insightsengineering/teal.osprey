6d4937340630518196c53a20bb7a3f93e528c20b
Mahmoud Hallal <mahmoud.hallal@roche.com>
2022-01-25 13:19:51 +0100

    remove sorting

--- R/tm_g_ae_oview.R
+++ R/tm_g_ae_oview.R
@@ -300,10 +300,9 @@ srv_g_ae_oview <- function(input,
       ref <- .(input$arm_ref)
       anl_labels <- rtables::var_labels(.(as.name(anl_name)))
       flag_labels <- anl_labels[names(anl_labels) %in% .(input$flag_var_anl)]
-      sorted_flag_labels <- flag_labels[order(match(names(flag_labels), .(input$flag_var_anl)))]
       flags <- .(as.name(anl_name)) %>%
         select(all_of(.(input$flag_var_anl))) %>%
-        rename_at(vars(.(input$flag_var_anl)), function(x) paste0(x, ": ", sorted_flag_labels[x]))
+        rename_at(vars(.(input$flag_var_anl)), function(x) paste0(x, ": ", flag_labels[x]))
     }))
 
     chunks_push_new_line()
