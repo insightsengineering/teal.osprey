2b6320cf92133f49795295224bcce89abee1231e
Junlue Zhao <jzhao31415@gmail.com>
2022-03-29 12:16:53 -0500

    added teal.data to package.R file

--- R/teal.osprey.R
+++ R/teal.osprey.R
@@ -2,7 +2,7 @@
 #'
 #' The \code{teal.osprey} R package contains interactive \code{\link{teal}} modules for the outputs
 #'
-#' @import dplyr osprey shiny teal formatters
+#' @import dplyr osprey shiny teal formatters teal.data
 #' @docType package
 #' @name teal.osprey
 #' @keywords internal
