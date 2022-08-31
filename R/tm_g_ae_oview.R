d5412f2157c994eb37a34d299f6270a0de31fec1
Blazewim <marek.blazewicz@contractors.roche.com>
2022-08-31 14:15:02 +0200

    value in variable check

--- R/tm_g_ae_oview.R
+++ R/tm_g_ae_oview.R
@@ -223,8 +223,12 @@ srv_g_ae_oview <- function(id,
 
   moduleServer(id, function(input, output, session) {
     iv <- shinyvalidate::InputValidator$new()
-    iv$add_rule("arm_var", shinyvalidate::sv_required(message = "Please select an arm variable."))
-    iv$add_rule("flag_var_anl", shinyvalidate::sv_required(message = "Please select at least one flag"))
+    iv$add_rule("arm_var", shinyvalidate::sv_required(
+      message = "Please select an arm variable.")
+      )
+    iv$add_rule("flag_var_anl", shinyvalidate::sv_required(
+      message = "Please select at least one flag")
+      )
     iv$enable()
 
     teal.code::init_chunks()
