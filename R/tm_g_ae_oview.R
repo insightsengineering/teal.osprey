e9175a094f922f5eb8a5242785ec42b79f6d2790
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-11-23 12:42:01 +0100

    tm_g_ae_overview

--- R/tm_g_ae_oview.R
+++ R/tm_g_ae_oview.R
@@ -205,11 +205,12 @@ srv_g_ae_oview <- function(id,
                            plot_width) {
   with_reporter <- !missing(reporter) && inherits(reporter, "Reporter")
   with_filter <- !missing(filter_panel_api) && inherits(filter_panel_api, "FilterPanelAPI")
-  checkmate::assert_class(data, "tdata")
+  checkmate::assert_class(data, "reactive")
+  checkmate::assert_class(isolate(data()), "teal_data")
 
   moduleServer(id, function(input, output, session) {
     iv <- reactive({
-      ANL <- data[[dataname]]() # nolint
+      ANL <- data()[[dataname]] # nolint
 
       iv <- shinyvalidate::InputValidator$new()
       iv$add_rule("arm_var", shinyvalidate::sv_required(
@@ -255,7 +256,7 @@ srv_g_ae_oview <- function(id,
     })
 
     observeEvent(input$arm_var, ignoreNULL = TRUE, {
-      ANL <- data[[dataname]]() # nolint
+      ANL <- data()[[dataname]] # nolint
       arm_var <- input$arm_var
       arm_val <- ANL[[arm_var]]
       choices <- levels(arm_val)
@@ -283,7 +284,7 @@ srv_g_ae_oview <- function(id,
     output_q <- shiny::debounce(
       millis = 200,
       r = reactive({
-        ANL <- data[[dataname]]() # nolint
+        ANL <- data()[[dataname]] # nolint
 
         teal::validate_has_data(ANL, min_nrow = 10, msg = sprintf("%s has not enough data", dataname))
 
@@ -295,7 +296,7 @@ srv_g_ae_oview <- function(id,
         ))
 
         q1 <- teal.code::eval_code(
-          teal.code::new_qenv(tdata2env(data), code = get_code_tdata(data)),
+          data(),
           code = as.expression(c(
             bquote(anl_labels <- formatters::var_labels(.(as.name(dataname)), fill = FALSE)),
             bquote(flags <- .(as.name(dataname)) %>%
