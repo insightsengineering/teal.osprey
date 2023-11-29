ec0c6ea7cd5ba82df788fcc7b47b1af38c2e9613
kartikeya kirar <kirar.kartikeya1@gmail.com>
2023-11-29 13:02:53 +0530

    `get_code` to return concatenated code string (#244)
    part of https://github.com/insightsengineering/teal.code/pull/176

--- R/tm_g_events_term_id.R
+++ R/tm_g_events_term_id.R
@@ -392,7 +392,7 @@ srv_g_events_term_id <- function(id,
           card$append_text("Comment", "header3")
           card$append_text(comment)
         }
-        card$append_src(paste(teal.code::get_code(output_q()), collapse = "\n"))
+        card$append_src(teal.code::get_code(output_q()))
         card
       }
       teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
