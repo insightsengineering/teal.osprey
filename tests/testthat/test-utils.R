a647b98cd432d52242861b3e35ebf7a5789f776e
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-01-23 16:42:02 +0100

    styling and bumps testthat version to a working one

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,15 +3,18 @@ testthat::test_that("as_numeric_from_comma_sep_str converts comma sep string int
   testthat::expect_equal(as_numeric_from_comma_sep_str("5,-6, 0"), c(5, -6, 0))
 })
 
-testthat::test_that("as_numeric_from_comma_sep_str throws warning if any entries are not numeric and converts those to NAs", {
-  warning_msg <- "NAs introduced by coercion"
-  testthat::expect_warning(result <- as_numeric_from_comma_sep_str("hello"), warning_msg)
-  testthat::expect_equal(result, as.numeric(NA))
-  testthat::expect_warning(result <- as_numeric_from_comma_sep_str("5,6,,hello,3"), warning_msg)
-  testthat::expect_equal(result, c(5, 6, NA, NA, 3))
-  testthat::expect_warning(result <- as_numeric_from_comma_sep_str("6f, 0.14,  f;"), warning_msg)
-  testthat::expect_equal(result, c(NA, 0.14, NA))
-})
+testthat::test_that(
+  "as_numeric_from_comma_sep_str throws warning if any entries are not numeric and converts those to NAs",
+  {
+    warning_msg <- "NAs introduced by coercion"
+    testthat::expect_warning(result <- as_numeric_from_comma_sep_str("hello"), warning_msg)
+    testthat::expect_equal(result, as.numeric(NA))
+    testthat::expect_warning(result <- as_numeric_from_comma_sep_str("5,6,,hello,3"), warning_msg)
+    testthat::expect_equal(result, c(5, 6, NA, NA, 3))
+    testthat::expect_warning(result <- as_numeric_from_comma_sep_str("6f, 0.14,  f;"), warning_msg)
+    testthat::expect_equal(result, c(NA, 0.14, NA))
+  }
+)
 
 testthat::test_that("as_numeric_from_comma_sep_str returns NULL if input is NULL or whitespace", {
   testthat::expect_null(as_numeric_from_comma_sep_str(NULL))
