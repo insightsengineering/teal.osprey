be6bcdfb85b8b5996c1f5d66b261ad65027484a4
kartikeya kirar <kirar.kartikeya1@gmail.com>
2024-03-14 16:54:34 +0530

    Update R/utils.R
    Co-authored-by: Marcin <133694481+m7pr@users.noreply.github.com>
    Signed-off-by: kartikeya kirar <kirar.kartikeya1@gmail.com>

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,7 @@
 #' @description Contains arguments that are shared between multiple functions
 #'   in the package to avoid repetition using `inheritParams`.
 #'
-#' @param plot_height (`numeric`) optional, a vector of length three with `c(value, min, max)`. Specifies
+#' @param plot_height (`numeric`) optional vector of length three with `c(value, min, max)`. Specifies
 #'   the height of the main plot.
 #' @param plot_width (`numeric`) optional vector of length three with `c(value, min, max)`. Specifies
 #'   the width of the main plot and renders a slider on the plot to interactively adjust the plot width.
