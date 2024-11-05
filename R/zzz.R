45e8f1f67f8681ddb76c537de26b3836061b1013
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-11-05 18:56:53 +0000

    fix: note on R CMD check

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,8 @@
 .onLoad <- function(libname, pkgname) {
+  # Fixes R CMD check note on "All declared Imports should be used."
+  # teal.data is necessary to access S3 method names.teal_data
+  teal.data::teal_data
+
   teal.logger::register_logger(namespace = "teal.osprey")
   teal.logger::register_handlers("teal.osprey")
 }
