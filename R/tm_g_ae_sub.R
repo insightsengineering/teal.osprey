29248cb7573fac2557717a0326f9b22658a75939
Vedha Viyash <49812166+vedhav@users.noreply.github.com>
2025-07-24 22:26:59 +0530

    Use the the new add button for adding a card (#310)
    Part of https://github.com/insightsengineering/coredev-tasks/issues/650

--- R/tm_g_ae_sub.R
+++ R/tm_g_ae_sub.R
@@ -106,7 +106,8 @@ ui_g_ae_sub <- function(id, ...) {
     ),
     encoding = tags$div(
       ### Reporter
-      teal.reporter::simple_reporter_ui(ns("simple_reporter")),
+      teal.reporter::add_card_button_ui(ns("add_reporter"), label = "Add Report Card"),
+      tags$br(), tags$br(),
       ###
       tags$label("Encodings", class = "text-primary"),
       helpText("Analysis data:", tags$code("ADAE")),
@@ -400,7 +401,7 @@ srv_g_ae_sub <- function(id,
         card$append_src(teal.code::get_code(output_q()))
         card
       }
-      teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
+      teal.reporter::add_card_button_srv("add_reporter", reporter = reporter, card_fun = card_fun)
     }
   })
 }
