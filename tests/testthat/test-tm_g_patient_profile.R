78d9a335a8da1080773558ce95a3cca1ba78aa18
Lluís Revilla Sancho <lluis.revilla_sancho@roche.com>
2026-07-27 15:52:04 +0200

    Add more tests

--- tests/testthat/test-tm_g_patient_profile.R
+++ tests/testthat/test-tm_g_patient_profile.R
@@ -18,33 +18,41 @@ sl_start_date_picks <- teal.picks::variables(
   selected = 1L
 )
 
-testthat::describe("tm_g_patient_profile input validation", {
+ex_var_cs <- teal.transform::choices_selected(
+  selected = "PARCAT2",
+  choices = "PARCAT2"
+)
+
+ex_var_picks <- variables(
+  selected = "PARCAT2",
+  choices = "PARCAT2"
+)
+
+describe("tm_g_patient_profile input validation", {
   it("plot arguments input validation", {
-    testthat::expect_error(
+    expect_error(
       {
         suppressWarnings(
           tm_g_patient_profile(
             label = "Patient Profile Plot",
             patient_id = patient_id_cs,
             sl_dataname = "ADSL",
-            sl_start_date = sl_start_date_cs,
-            plot_height = c(1200, 5000, 400)
+            sl_start_date = sl_start_date_cs
           ),
-          classes = "picks_delayed"
+          classes = c("picks_delayed", "lifecycle_warning_deprecated")
         )
       },
       "Assertion on 'plot_height' failed"
     )
 
-    testthat::expect_error(
+    expect_error(
       {
         suppressWarnings(
           tm_g_patient_profile(
             label = "Patient Profile Plot",
             patient_id = patient_id_cs,
             sl_dataname = "ADSL",
-            sl_start_date = sl_start_date_cs,
-            plot_width = c(1200, 5000, 400)
+            sl_start_date = sl_start_date_cs
           ),
           classes = "picks_delayed"
         )
@@ -54,7 +62,7 @@ testthat::describe("tm_g_patient_profile input validation", {
   })
 
   it("Forcing conversion from multiple picks to single", {
-    testthat::expect_error(
+    expect_error(
       {
         suppressWarnings(
           tm_g_patient_profile(
@@ -67,13 +75,13 @@ testthat::describe("tm_g_patient_profile input validation", {
             sl_dataname = "ADSL",
             sl_start_date = sl_start_date_cs
           ),
-          classes = "picks_delayed"
+          classes = c("picks_delayed", "lifecycle_warning_deprecated")
         )
       },
       "metadata does not match the requirement for patient_id"
     )
 
-    testthat::expect_error(
+    expect_error(
       {
         suppressWarnings(
           tm_g_patient_profile(
@@ -86,7 +94,7 @@ testthat::describe("tm_g_patient_profile input validation", {
               multiple = TRUE
             )
           ),
-          classes = "picks_delayed"
+          classes = c("picks_delayed", "lifecycle_warning_deprecated")
         )
       },
       "metadata does not match the requirement for sl_start_date"
@@ -94,28 +102,88 @@ testthat::describe("tm_g_patient_profile input validation", {
   })
 })
 
-testthat::describe("tm_g_patient_profile module creation", {
+describe("tm_g_patient_profile module creation", {
   it("creates a teal module using choices_selected", {
-    mod <- tm_g_patient_profile(
+    mod <- suppressWarnings(tm_g_patient_profile(
       label = "Patient Profile Plot",
       patient_id = patient_id_cs,
       sl_dataname = "ADSL",
-      sl_start_date = sl_start_date_cs,
-      plot_height = c(1200, 400, 5000)
-    ) |>
-      suppressWarnings(classes = "picks_delayed")
-    testthat::expect_s3_class(mod, "teal_module")
+      sl_start_date = sl_start_date_cs
+    ), classes = c("picks_delayed", "lifecycle_warning_deprecated"))
+    expect_s3_class(mod, "teal_module")
   })
 
   it("creates a teal module using teal.picks variables", {
-    mod <- tm_g_patient_profile(
+    mod <- suppressWarnings(tm_g_patient_profile(
       label = "Patient Profile Plot",
       patient_id = patient_id_picks,
       sl_dataname = "ADSL",
+      sl_start_date = sl_start_date_picks
+    ), classes = "picks_delayed")
+    expect_s3_class(mod, "teal_module")
+  })
+
+
+  data <- within(teal_data(), {
+    library(nestcolor)
+    library(dplyr)
+    ADSL <- rADSL
+    ADAE <- rADAE %>% mutate(ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
+    ADCM <- rADCM %>% mutate(ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
+    # The step below is to pre-process ADCM to legacy standard
+    ADCM <- ADCM %>%
+      select(-starts_with("ATC")) %>%
+      unique()
+    ADRS <- rADRS %>% mutate(ADT = as.Date(ADTM))
+    ADEX <- rADEX %>% mutate(ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
+    ADLB <- rADLB %>% mutate(ADT = as.Date(ADTM), LBSTRESN = as.numeric(LBSTRESC))
+  })
+
+  join_keys(data) <- default_cdisc_join_keys[names(data)]
+
+  it("works using choices_selected", {
+    mod <- suppressWarnings(
+      tm_g_patient_profile(
+        label = "Patient Profile Plot",
+        patient_id = patient_id_cs,
+        sl_dataname = "ADSL",
+        ex_dataname = "ADEX",
+        ex_var = ex_var_cs,
+        sl_start_date = sl_start_date_cs
+      ), classes = c("picks_delayed", "lifecycle_warning_deprecated"))
+
+    # expect_warning(
+    testServer(
+      mod$server,
+      args = c(list(id = "test_id", data = shiny::reactive(data)), mod$server_args),
+      expr = {
+        session$setInput(select_ADaM = c("ADEX"))
+        # browser()
+        expect_no_error(session$returned())
+      }
+    )
+    #   regexp = "cartesian join"
+    # )
+
+  })
+
+  it("works using teal.picks variables", {
+    mod <- suppressWarnings(tm_g_patient_profile(
+      label = "Patient Profile Plot",
+      patient_id = patient_id_picks,
+      sl_dataname = "ADSL",
+      ex_dataname = "ADEX",
       sl_start_date = sl_start_date_picks,
-      plot_height = c(1200, 400, 5000)
-    ) |>
-      suppressWarnings(classes = "picks_delayed")
-    testthat::expect_s3_class(mod, "teal_module")
+      ex_var = ex_var_picks
+    ), classes = "picks_delayed")
+
+    testServer(
+      mod$server,
+      args = c(list(id = "test_id", data = shiny::reactive(data)), mod$server_args),
+      expr = {
+        session$setInput(select_ADaM = "ADEX")
+        expect_no_error(session$returned())
+      }
+    )
   })
 })
