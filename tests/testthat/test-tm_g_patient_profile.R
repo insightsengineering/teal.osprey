e92a9039460e9046ae42eb688713061ed057dae1
Lluís Revilla Sancho <lluis.revilla_sancho@roche.com>
2026-07-28 19:02:01 +0200

    Fix tests

--- tests/testthat/test-tm_g_patient_profile.R
+++ tests/testthat/test-tm_g_patient_profile.R
@@ -1,31 +1,31 @@
 patient_id_cs <- teal.transform::choices_selected(
-  selected = "USUBJID",
-  choices = c("USUBJID")
+  choices = "USUBJID",
+  selected = "USUBJID"
 )
 
 sl_start_date_cs <- teal.transform::choices_selected(
-  selected = "TRTSDTM",
-  choices = c("TRTSDTM", "RANDDT")
+  choices = c("TRTSDTM", "RANDDT"),
+  selected = "TRTSDTM"
 )
 
 patient_id_picks <- teal.picks::variables(
-  choices = dplyr::where(is.character),
-  selected = 1L
+  choices = "USUBJID",
+  selected = "USUBJID"
 )
 
 sl_start_date_picks <- teal.picks::variables(
-  choices = dplyr::where(function(x) inherits(x, c("Date", "POSIXct", "POSIXt"))),
-  selected = 1L
+  choices = c("TRTSDTM", "RANDDT"),
+  selected = "TRTSDTM"
 )
 
 ex_var_cs <- teal.transform::choices_selected(
-  selected = "PARCAT2",
-  choices = "PARCAT2"
+  choices = "PARCAT2",
+  selected = "PARCAT2"
 )
 
 ex_var_picks <- variables(
-  selected = "PARCAT2",
-  choices = "PARCAT2"
+  choices = "PARCAT2",
+  selected = "PARCAT2"
 )
 
 describe("tm_g_patient_profile input validation", {
@@ -37,7 +37,8 @@ describe("tm_g_patient_profile input validation", {
             label = "Patient Profile Plot",
             patient_id = patient_id_cs,
             sl_dataname = "ADSL",
-            sl_start_date = sl_start_date_cs
+            sl_start_date = sl_start_date_cs,
+            plot_height = c(1200, 5000, 400)
           ),
           classes = c("picks_delayed", "lifecycle_warning_deprecated")
         )
@@ -52,7 +53,8 @@ describe("tm_g_patient_profile input validation", {
             label = "Patient Profile Plot",
             patient_id = patient_id_cs,
             sl_dataname = "ADSL",
-            sl_start_date = sl_start_date_cs
+            sl_start_date = sl_start_date_cs,
+            plot_width = c(1200, 5000, 400)
           ),
           classes = "picks_delayed"
         )
@@ -108,7 +110,8 @@ describe("tm_g_patient_profile module creation", {
       label = "Patient Profile Plot",
       patient_id = patient_id_cs,
       sl_dataname = "ADSL",
-      sl_start_date = sl_start_date_cs
+      sl_start_date = sl_start_date_cs,
+      plot_height = c(1200, 400, 5000)
     ), classes = c("picks_delayed", "lifecycle_warning_deprecated"))
     expect_s3_class(mod, "teal_module")
   })
@@ -118,25 +121,25 @@ describe("tm_g_patient_profile module creation", {
       label = "Patient Profile Plot",
       patient_id = patient_id_picks,
       sl_dataname = "ADSL",
-      sl_start_date = sl_start_date_picks
+      sl_start_date = sl_start_date_picks,
+      plot_height = c(1200, 400, 5000)
     ), classes = "picks_delayed")
     expect_s3_class(mod, "teal_module")
   })
-
 
   data <- within(teal_data(), {
     library(nestcolor)
     library(dplyr)
     ADSL <- rADSL
-    ADAE <- rADAE %>% mutate(ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
-    ADCM <- rADCM %>% mutate(ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
+    ADAE <- mutate(rADAE, ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
+    ADCM <- mutate(rADCM, ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
     # The step below is to pre-process ADCM to legacy standard
     ADCM <- ADCM %>%
       select(-starts_with("ATC")) %>%
       unique()
-    ADRS <- rADRS %>% mutate(ADT = as.Date(ADTM))
-    ADEX <- rADEX %>% mutate(ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
-    ADLB <- rADLB %>% mutate(ADT = as.Date(ADTM), LBSTRESN = as.numeric(LBSTRESC))
+    ADRS <- mutate(rADRS, ADT = as.Date(ADTM))
+    ADEX <- mutate(rADEX, ASTDT = as.Date(ASTDTM), AENDT = as.Date(AENDTM))
+    ADLB <- mutate(rADLB, ADT = as.Date(ADTM), LBSTRESN = as.numeric(LBSTRESC))
   })
 
   join_keys(data) <- default_cdisc_join_keys[names(data)]
@@ -149,23 +152,21 @@ describe("tm_g_patient_profile module creation", {
         sl_dataname = "ADSL",
         ex_dataname = "ADEX",
         ex_var = ex_var_cs,
-        sl_start_date = sl_start_date_cs
-      ),
-      classes = c("picks_delayed", "lifecycle_warning_deprecated")
-    )
+        sl_start_date = sl_start_date_cs,
+        plot_height = c(1200, 400, 5000)
+      ), classes = c("picks_delayed", "lifecycle_warning_deprecated"))
 
-    # expect_warning(
-    testServer(
-      mod$server,
-      args = c(list(id = "test_id", data = shiny::reactive(data)), mod$server_args),
-      expr = {
-        session$setInput(select_ADaM = c("ADEX"))
-        # browser()
-        expect_no_error(session$returned())
-      }
+    expect_warning(
+      testServer(
+        mod$server,
+        args = c(list(id = "test_id", data = shiny::reactive(data)), mod$server_args),
+        expr = {
+          session$setInputs(select_ADaM = "ADEX", x_limit = "-28, 750")
+          expect_no_error(session$returned())
+        }
+      ),
+      regexp = "contains multiple values"
     )
-    #   regexp = "cartesian join"
-    # )
   })
 
   it("works using teal.picks variables", {
@@ -175,16 +176,20 @@ describe("tm_g_patient_profile module creation", {
       sl_dataname = "ADSL",
       ex_dataname = "ADEX",
       sl_start_date = sl_start_date_picks,
-      ex_var = ex_var_picks
+      ex_var = ex_var_picks,
+      plot_height = c(1200, 400, 5000)
     ), classes = "picks_delayed")
 
-    testServer(
-      mod$server,
-      args = c(list(id = "test_id", data = shiny::reactive(data)), mod$server_args),
-      expr = {
-        session$setInput(select_ADaM = "ADEX")
-        expect_no_error(session$returned())
-      }
+    expect_warning(
+      testServer(
+        mod$server,
+        args = c(list(id = "test_id", data = shiny::reactive(data)), mod$server_args),
+        expr = {
+          session$setInputs(select_ADaM = "ADEX", x_limit = "-28, 750")
+          expect_no_error(session$returned())
+        }
+      ),
+      regexp = "contains multiple values"
     )
   })
 })
