16080f930841fb077b4de78ada899a008fe7d683
Oriol Senan <35930244+osenan@users.noreply.github.com>
2026-08-12 15:34:42 +0200

    Update R/tm_g_butterfly.R
    Co-authored-by: André Veríssimo <211358+averissimo@users.noreply.github.com>
    Signed-off-by: Oriol Senan  <35930244+osenan@users.noreply.github.com>

--- R/tm_g_butterfly.R
+++ R/tm_g_butterfly.R
@@ -126,8 +126,8 @@
 tm_g_butterfly <- function(label,
                            dataname,
                            filter_var = NULL,
-                           right_var = teal.picks::variables(function(x) is.factor(x)),
-                           left_var = teal.picks::variables(function(x) is.factor(x)),
+                           right_var = teal.picks::variables(is.factor),
+                           left_var = teal.picks::variables(is.factor),
                            category_var = teal.picks::variables(teal.picks::is_categorical()),
                            color_by_var = teal.picks::variables(dplyr::starts_with("AETO")),
                            count_by_var = values(
